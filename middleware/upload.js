44605337098bccecfafbc336ab411108f2d73d61
samtronicssolutions-source <samtronics.solutions@gmail.com>
2026-03-31 06:28:27 -0700

    Update upload.js

--- middleware/upload.js
+++ middleware/upload.js
@@ -2,7 +2,8 @@ const multer = require('multer');
 const path = require('path');
 const fs = require('fs');
 
-const uploadDir = 'uploads/products';
+// Save to public/images/products
+const uploadDir = 'public/images/products';
 if (!fs.existsSync(uploadDir)) {
   fs.mkdirSync(uploadDir, { recursive: true });
 }
